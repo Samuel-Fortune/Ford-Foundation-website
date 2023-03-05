4819e3bffcaab65b3a272215e2b863c8cda5037b
sanusiaz <sanusiabdulazeezadebayo1@gmail.com>
2023-03-05 20:42:41 +0100

    Home Page Footer pushed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,css}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ['']
+      }
+    },
   },
   plugins: [],
 }
